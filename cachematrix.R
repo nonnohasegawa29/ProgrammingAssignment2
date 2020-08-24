fc62b8bf79ff43727e5bb93336c92b4e0c3f61d7
nonnohasegawa29 <70122446+nonnohasegawa29@users.noreply.github.com>
2020-08-24 12:41:00 +0900

    Update cachematrix.R
    This is my assignment for week 3 programming in R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makecachematrix and cachesolve will output you with inverse of matrix inputted
+## this was our assignment for R programming week3 
 
-## Write a short comment describing this function
+
+## make cache matrix first
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function (y) {
+                x <<- y 
+                s <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) s <<- inverse
+        getinverse <- function() s
+        list(set=set,
+             get=get,
+             setinverse=setinverse,
+             getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## then use cacheSolve to solve the cache inverse of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinverse()
+        if (!is.null(s)) {
+                message("Getting Cached Data...")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setinverse(s)
+        s
 }
+        ## Return a matrix that is the inverse of 'x'
+
+
+##To use this simply run
+
+A<- matrix(c(2,3,4,5,6,7,8,9,1),3,3)
+B <- makeCacheMatrix(A)
+cacheSolve(B)
+
